84145444367dd74dbc5ca1b24b026c9f04e207a7
Parham113e <149345117+Parham113e@users.noreply.github.com>
2026-02-05 05:59:15 -0800

    Update Section.jsx

--- app/components/Section.jsx
+++ app/components/Section.jsx
@@ -1,1 +1,15 @@
+export function Section({ children, width = "900px", center = false }) {
+  return (
+    <section
+      style={{
+        padding: "var(--space-2xl) var(--space-lg)",
+        maxWidth: width,
+        margin: "0 auto",
+        textAlign: center ? "center" : "left"
+      }}
+    >
+      {children}
+    </section>
+  );
+}
 
