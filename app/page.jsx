6d62a4aee43aec8a1475a4bcedafa920ef4bb9e1
Parham113e <149345117+Parham113e@users.noreply.github.com>
2026-02-05 07:21:12 -0600

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Section } from "./components/Section";
+import { SectionHeader } from "./components/SectionHeader";
+import { FeatureCard } from "./components/FeatureCard";
+import { StepCard } from "./components/StepCard";
+import { Button } from "./components/Button";
